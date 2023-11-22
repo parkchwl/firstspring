df1211230b71449924715e986be8bbf7b5c98c0b
parkchwl <93250361+parkchwl@users.noreply.github.com>
2023-11-23 00:41:03 +0900

    spring 02
    Lombok을 활용한 리팩토링
    [코드 단축 & 로깅 기능]

--- src/main/java/com/example/firstspring/controller/ArticleController.java
+++ src/main/java/com/example/firstspring/controller/ArticleController.java
@@ -3,12 +3,14 @@ package com.example.firstspring.controller;
 import com.example.firstspring.controller.repository.ArticleRepository;
 import com.example.firstspring.dto.ArticleForm;
 import com.example.firstspring.entity.Article;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 
 @Controller
+@Slf4j // 로깅을 위한 어노테이션
 public class ArticleController {
     @Autowired// 스프링부트가 미리 생성해놓은 객체를 가져다가 자동 연결해준다.
     private ArticleRepository articleRepository;
@@ -20,15 +22,18 @@ public class ArticleController {
 
     @PostMapping("/articles/create")
     public String createArticle(ArticleForm form){
-        System.out.println(form.toString());
+        log.info(form.toString());
+        //System.out.println(form.toString()); -> 로깅 기능을 사용해야 한다.
 
         // 1. Dto를 Entity로 변환.
         Article article = form.toEntity();
-        System.out.println(article.toString());
+        //System.out.println(article.toString());
+        log.info(article.toString());
 
         // 2, Repository에 Entity를 Db안에 저장한다.
         Article saved = articleRepository.save(article);
-        System.out.println(saved.toString());
+        //System.out.println(saved.toString());
+        log.info(saved.toString());
 
         return"";
     }
--- src/main/java/com/example/firstspring/entity/Article.java
+++ src/main/java/com/example/firstspring/entity/Article.java
@@ -1,11 +1,16 @@
 package com.example.firstspring.entity;
 
+import lombok.AllArgsConstructor;
+import lombok.ToString;
+
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
 
 @Entity // @Entity를 해야 DB가 해당 객체를 인식 가능하다
+@AllArgsConstructor //id, title, content를 파라미터로 하는 생성자를 만들어준다.
+@ToString
 public class Article {
     @Id // 대표값을 지정
     @GeneratedValue // 1, 2, 3, ... 자동 어노테이션
@@ -15,23 +20,7 @@ public class Article {
     private String title;
     @Column //DB에서 이해할 수 있게 @Conlumn 어노테이션
     private String content;
+    public Article() {
 
-    public Article(Long id, String title, String content) {
-        this.id = id;
-        this.title = title;
-        this.content = content;
-    }
-
-    public Article () {
-
-    }
-
-    @Override
-    public String toString() {
-        return "Article{" +
-                "id=" + id +
-                ", title='" + title + '\'' +
-                ", content='" + content + '\'' +
-                '}';
     }
 }
