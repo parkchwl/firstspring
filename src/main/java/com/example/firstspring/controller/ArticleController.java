6e477bfcafa2ab4cbf514456b83e8e2be271a4a8
parkchwl <93250361+parkchwl@users.noreply.github.com>
2023-11-26 19:55:46 +0900

    spring 03
    데이터를 조회하기 위한 URL 요청 파라미터 사용과 반환 & 모델에 데이터를 등록하고 View페이지에서 조회

--- src/main/java/com/example/firstspring/controller/ArticleController.java
+++ src/main/java/com/example/firstspring/controller/ArticleController.java
@@ -6,7 +6,9 @@ import com.example.firstspring.entity.Article;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 
 @Controller
@@ -36,5 +38,18 @@ public class ArticleController {
         log.info(saved.toString());
 
         return"";
+    }
+    @GetMapping("/articles/{id}")
+    public String show(@PathVariable Long id, Model model){
+        log.info("id + = "+ id);
+
+        // 1 : id를 데이터로 가져옴
+        Article articleEntity = articleRepository.findById(id).orElse(null);
+
+        // 2 : 가져온 데이터를 모델에 등록한다.
+        model.addAttribute("article", articleEntity);
+
+        // 3 : 보여줄 페이지를 설정 / 반환한다.
+        return "articles/show"; // View 페이지에서 쓸 수 있다.
     }
 }
--- src/main/java/com/example/firstspring/entity/Article.java
+++ src/main/java/com/example/firstspring/entity/Article.java
@@ -1,6 +1,7 @@
 package com.example.firstspring.entity;
 
 import lombok.AllArgsConstructor;
+import lombok.NoArgsConstructor;
 import lombok.ToString;
 
 import javax.persistence.Column;
@@ -10,6 +11,7 @@ import javax.persistence.Id;
 
 @Entity // @Entity를 해야 DB가 해당 객체를 인식 가능하다
 @AllArgsConstructor //id, title, content를 파라미터로 하는 생성자를 만들어준다.
+@NoArgsConstructor // 디폴트 생성자 추가를 위한 어노테이션
 @ToString
 public class Article {
     @Id // 대표값을 지정
@@ -20,7 +22,5 @@ public class Article {
     private String title;
     @Column //DB에서 이해할 수 있게 @Conlumn 어노테이션
     private String content;
-    public Article() {
 
-    }
 }
