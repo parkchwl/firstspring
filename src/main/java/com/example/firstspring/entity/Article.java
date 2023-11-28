0b783f5cfea3c934beaac360240bcb727ea59096
parkchwl <93250361+parkchwl@users.noreply.github.com>
2023-11-28 18:34:32 +0900

    spring 05
    CRUD & SQL 이해

--- src/main/java/com/example/firstspring/entity/Article.java
+++ src/main/java/com/example/firstspring/entity/Article.java
@@ -5,10 +5,7 @@ import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.ToString;
 
-import javax.persistence.Column;
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.Id;
+import javax.persistence.*;
 
 @Entity // @Entity를 해야 DB가 해당 객체를 인식 가능하다
 @AllArgsConstructor //id, title, content를 파라미터로 하는 생성자를 만들어준다.
@@ -17,7 +14,7 @@ import javax.persistence.Id;
 @Getter // 모든 게터를 추가.
 public class Article {
     @Id // 대표값을 지정
-    @GeneratedValue // 1, 2, 3, ... 자동 어노테이션
+    @GeneratedValue (strategy = GenerationType.IDENTITY)// 1, 2, 3, ... 자동 어노테이션
     private Long id; //대표값
 
     @Column //DB에서 이해할 수 있게 @Conlumn 어노테이션
