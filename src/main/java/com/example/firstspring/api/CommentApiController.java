f69f3adb2be186179a0db51f551eb63e0a48d470
parkchwl <93250361+parkchwl@users.noreply.github.com>
2023-12-04 13:34:51 +0900

    spring 11
    댓글 서비스 & 댓글 컨트롤러 구현

--- src/main/java/com/example/firstspring/api/CommentApiController.java
+++ src/main/java/com/example/firstspring/api/CommentApiController.java
@@ -5,9 +5,7 @@ import com.example.firstspring.service.CommentService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -26,9 +24,29 @@ public class CommentApiController {
         return ResponseEntity.status(HttpStatus.OK).body(dtos);
     }
 
-    // 댓글 생성
+    // 댓글
+    @PostMapping("/api/articles/{articleId}/comments")
+    public ResponseEntity<CommentDto> create(@PathVariable Long articleId,
+                                             @RequestBody CommentDto dto) throws IllegalAccessException {
+        // 서비스에게 위임
+        CommentDto createdDto= commentService.create(articleId, dto);
+        // 결과 응답
+
+        return ResponseEntity.status(HttpStatus.OK).body(createdDto);
+    }
 
     // 댓글 수정
+    @PatchMapping("/api/comments/{id}")
+    public ResponseEntity<CommentDto> update(@PathVariable Long id, @RequestBody CommentDto dto) {
 
+        CommentDto updatedDto = commentService.update(id, dto);
+        return ResponseEntity.status(HttpStatus.OK).body(updatedDto);
+    }
     // 댓글 삭제
+    @DeleteMapping("/api/comments/{id}")
+    public ResponseEntity<CommentDto> delete(@PathVariable Long id) {
+
+        CommentDto updatedDto = commentService.delete(id);
+        return ResponseEntity.status(HttpStatus.OK).body(updatedDto);
+    }
 }
--- src/main/java/com/example/firstspring/dto/CommentDto.java
+++ src/main/java/com/example/firstspring/dto/CommentDto.java
@@ -1,5 +1,7 @@
 package com.example.firstspring.dto;
 
+import com.example.firstspring.entity.Comment;
+import com.fasterxml.jackson.annotation.JsonProperty;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -11,7 +13,17 @@ import lombok.ToString;
 @ToString
 public class CommentDto {
     private Long id;
+    @JsonProperty("article_id")
     private Long articleId;
     private String nickname;
     private String body;
+
+    public static CommentDto createCommentDto(Comment comment) {
+        return new CommentDto(
+                comment.getId(),
+                comment.getArticle().getId(),
+                comment.getNickname(),
+                comment.getBody()
+        );
+    }
 }
